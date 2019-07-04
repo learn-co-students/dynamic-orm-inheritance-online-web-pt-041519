9459e2ab67615507d70d7a3b6961ef3d780b9660
Katherine Jablonski <kjablonski08@gmail.com>
2019-07-04 01:38:18 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,8 +49,7 @@ class InteractiveRecord
   end
 
 def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-  DB[:conn].execute(sql, name)
+DB[:conn].execute("SELECT * FROM table_name = ? WHERE name = ?", self.table_name, name)
 end
 
 end
